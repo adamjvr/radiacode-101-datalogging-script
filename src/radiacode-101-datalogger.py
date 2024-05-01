757e353a36c4c9c58e3b9445fdbc558123f1e254
Adam Vadala-Roth <fenderstratman@gmail.com>
2024-05-01 17:08:53 -0400

    Radiacode device is now connecting to system over USB, script updated to use Bluetooth if user desires so. Datalogging portion still producing errors but this is significant progress since yesterday

--- src/radiacode-101-datalogger.py
+++ src/radiacode-101-datalogger.py
@@ -1,9 +1,11 @@
+import argparse
 import csv  # Importing the CSV module for handling CSV files
 import time  # Importing the time module for timestamping
-import radiacode  # Importing the radiacode library for interfacing with the Radiacode 101 device
+from radiacode import RadiaCode  # Importing the RadiaCode class from the radiacode library
+
 
 # Function to sample radiation data and record it in a CSV file
-def sample_radiation_data(num_samples, sample_interval, csv_filename):
+def sample_radiation_data(num_samples, sample_interval, csv_filename,radiacode):
     # Open the CSV file in write mode with newline='' to ensure proper line endings
     with open(csv_filename, 'w', newline='') as csvfile:
         # Define the field names for the CSV file
@@ -14,8 +16,7 @@ def sample_radiation_data(num_samples, sample_interval, csv_filename):
         writer.writeheader()
 
         # Initialize the Radiacode 101 device
-        # radiacode_device = Radiacode()
-        radiacode_device = radiacode(serial_number="RC-101-003059")
+        #radiacode_device = radiacode(serial_number="RC-101-003059")
 
         # Loop to sample radiation data for the specified number of samples
         for _ in range(num_samples):
@@ -30,6 +31,32 @@ def sample_radiation_data(num_samples, sample_interval, csv_filename):
 
 # Main function
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bluetooth-mac', type=str, required=False, help='bluetooth MAC address of radiascan device')
+    args = parser.parse_args()
+
+    if args.bluetooth_mac:
+        print('will use Bluetooth connection')
+        radiacode_device = RadiaCode(bluetooth_mac=args.bluetooth_mac)
+    else:
+        print('will use USB connection')
+        radiacode_device = RadiaCode()
+
+    serial = radiacode_device.serial_number()
+    print(f'### Serial number: {serial}')
+    print('--------')
+
+    fw_version = radiacode_device.fw_version()
+    print(f'### Firmware: {fw_version}')
+    print('--------')
+
+    spectrum = radiacode_device.spectrum()
+    print(f'### Spectrum: {spectrum}')
+    print('--------')
+
+    print('### DataBuf:')
+
+
     # Prompt the user to input the number of samples to take
     num_samples = int(input("Enter the number of samples to take: "))
     # Prompt the user to input the time interval between samples (in seconds)
@@ -38,4 +65,4 @@ if __name__ == "__main__":
     csv_filename = input("Enter the name of the CSV file to save data to: ")
 
     # Call the function to sample radiation data and record it in the CSV file
-    sample_radiation_data(num_samples, sample_interval, csv_filename)
+    sample_radiation_data(num_samples, sample_interval, csv_filename,radiacode_device)
